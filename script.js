fcf2f6f222c0fbc195471c8c652094fddb6fe5fb
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-18 09:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const bow = document.getElementById("browser-info");
+const bname = navigator.appName;
+const version = navigator.appVersion;
+const value = "You are using "+bname+" "+"version is "+version;
+// const ptag = document.createElement("p");
+// const cont = document.createTextNode("rahuls");
+// ptag.appendChild(cont);
+// bow.appendChild(ptag);
+// ptag.value = "rahul";
+// bow.appendChild(ptag);
+bow.append(value);
+
